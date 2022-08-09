c5274977eea208c12faf8f813d99a870df26ee35
bolsonitro <bolsonitro_bitcoiner@protonmail.com>
2022-08-08 19:13:19 -0700

    Try Catch

--- src/domain/usecases/getPrincipalPosition.js
+++ src/domain/usecases/getPrincipalPosition.js
@@ -19,15 +19,20 @@ const getPrincipalPosition = injection =>
     setup: ctx => (ctx.di = Object.assign({}, dependency, injection)),
 
     'Get the principal position and return': step(async ctx => {
-      const client = new RestClient(
-        process.env.PUBLIC_API_KEY,
-        process.env.PRIVATE_API_KEY
-      )
-      const positions = await client.getPositions(true)
-      const bolsonaroPosition = positions.result.find((position) => {
-        return position.future == "BOLSONARO2022";
-      });
-      return Ok(ctx.ret = bolsonaroPosition)
+      try {
+        const client = new RestClient(
+          process.env.PUBLIC_API_KEY,
+          process.env.PRIVATE_API_KEY
+        )
+        const positions = await client.getPositions(true)
+        const bolsonaroPosition = positions.result.find((position) => {
+          return position.future == "BOLSONARO2022";
+        });
+        return Ok(ctx.ret = bolsonaroPosition)
+      } catch (error) {
+        return Err(error)
+      }
+
     })
   })
 
